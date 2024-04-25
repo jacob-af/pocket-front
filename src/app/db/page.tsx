dc02a3afd446fd57ba95d5b60d776c5da4099874
jacob-af <jfeitler@gmail.com>
2024-04-25 00:44:28 -0400

    minor stiling

--- src/app/db/page.tsx
+++ src/app/db/page.tsx
@@ -1,6 +1,6 @@
 export default async function Landing() {
   return (
-    <div>
+    <div className="max-w-lg">
       This is a protected route.
       <br />
       You will only see this if you are authenticated.
