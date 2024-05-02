727ca68e3f22bd4d256fd14e77909783ecc0c7c0
jacob-af <jfeitler@gmail.com>
2024-05-02 07:21:37 -0400

    bottom margin

--- src/app/db/(protected routes)/recipe/page.tsx
+++ src/app/db/(protected routes)/recipe/page.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 
 export default function Recipe() {
   return (
-    <div className="min-h-screen flex flex-col content-center max-w-2xl">
+    <div className="min-h-screen flex flex-col content-center max-w-2xl overflow-auto">
       <RecipeDropDown />
       <RecipeCard />
       <RecipeLoader />
       <Link
         href="/db/recipe/add"
-        className="btn-secondary inline-block bg-gray-500 text-white px-5 py-3 rounded hover:bg-gray-600 mr-4"
+        className="btn-secondary inline-block bg-gray-500 text-white px-5 py-3 rounded hover:bg-gray-600 mr-4 mb-16"
       >
         Add Recipe
       </Link>
