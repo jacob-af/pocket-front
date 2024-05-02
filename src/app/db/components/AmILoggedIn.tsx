09c1224072403508982c392113a50b092f3d6528
jacob-af <jfeitler@gmail.com>
2024-05-02 08:06:18 -0400

    better name loading

---
+++ src/app/db/components/AmILoggedIn.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useSession, getSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+export default function AmILoggedIn() {
+  const { update } = useSession();
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    async function fetchSession() {
+      const session = await getSession();
+      if (session?.user && session.user.accessTokenExpires < Date.now()) {
+        update({ action: "New Tokens" });
+        console.log("new tokens");
+      } else if (session?.user) {
+        setUserName(session.user.name);
+      }
+    }
+    fetchSession();
+  });
+
+  return <div className="float float-left">Logged in as: {userName}</div>;
+}
