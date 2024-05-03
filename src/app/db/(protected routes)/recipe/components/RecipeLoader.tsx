67c8da7fa04da7e6395ec341e909deb5c090b277
jacob-af <jfeitler@gmail.com>
2024-05-03 19:14:35 -0400

    return of the memo

--- src/app/db/(protected routes)/recipe/components/RecipeLoader.tsx
+++ src/app/db/(protected routes)/recipe/components/RecipeLoader.tsx
@@ -9,37 +9,40 @@ import { useEffect, useMemo } from "react";
 
 export default function RecipeLoader() {
   const { status: sessionStatus } = useSession();
-  const userRecipes = useReactiveVar(userRecipeList);
   const { data, loading, error } = useQuery(USER_BUILDS, {
     skip: sessionStatus !== "authenticated",
     fetchPolicy: "cache-and-network"
   });
 
-  useEffect(() => {
-    const recipes: Recipe[] = [];
-    if (data && data.usersBuilds) {
-      data.usersBuilds.forEach(userBuild => {
-        const { recipe } = userBuild;
-        const index = recipes.findIndex(rec => rec.name === recipe.name);
-        if (index === -1) {
-          recipes.push({
-            ...recipe,
-            build: [userBuild]
-          });
-        } else {
-          recipes[index] = {
-            ...recipes[index],
-            build: [...recipes[index].build, { ...userBuild }]
-          };
-        }
-      }, []);
-
-      recipes.sort((a, b) => a.name.localeCompare(b.name));
-    }
-    userRecipeList(recipes);
-  }, [data]);
-
   // Memoized recipes array
+  const recipes = useMemo(() => {
+    if (!data?.usersBuilds) {
+      return [];
+    }
+    const recipes: Recipe[] = [];
+    data.usersBuilds.forEach(userBuild => {
+      const { recipe } = userBuild;
+      const index = recipes.findIndex(rec => rec.name === recipe.name);
+      if (index === -1) {
+        recipes.push({
+          ...recipe,
+          build: [userBuild]
+        });
+      } else {
+        recipes[index] = {
+          ...recipes[index],
+          build: [...recipes[index].build, { ...userBuild }]
+        };
+      }
+    });
+
+    recipes.sort((a, b) => a.name.localeCompare(b.name));
+    return recipes;
+  }, [data?.usersBuilds]);
+
+  useEffect(() => {
+    userRecipeList(recipes);
+  }, [recipes]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -49,5 +52,5 @@ export default function RecipeLoader() {
     return <div>{error.message}</div>;
   }
 
-  return <>{userRecipes.length} Recipes</>;
+  return <div>{`${recipes.length} Recipes Loaded`}</div>;
 }
