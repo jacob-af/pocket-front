7a8775b70322a6f348a0f20344f99c336f0446c3
jacob-af <jfeitler@gmail.com>
2024-05-04 23:15:08 -0400

    adjusting variables again

--- src/app/db/(protected routes)/recipe/components/RecipeLoader.tsx
+++ src/app/db/(protected routes)/recipe/components/RecipeLoader.tsx
@@ -1,57 +1,60 @@
 "use client";
-export const dynamic = "force-dynamic";
 
 import { Build, Recipe } from "@/__generated__/graphql";
 import { NetworkStatus, useQuery, useReactiveVar } from "@apollo/client";
-import { useEffect, useMemo } from "react";
 
 import { USER_BUILDS } from "@/app/graphql/queries/recipe";
+import { useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { userRecipeList } from "@/app/graphql/reactiveVar/recipes";
 
 export default function RecipeLoader() {
   const { status: sessionStatus } = useSession();
-  const { data, loading, error } = useQuery(USER_BUILDS, {
+  const { data, loading, error, refetch } = useQuery(USER_BUILDS, {
     skip: sessionStatus !== "authenticated",
     fetchPolicy: "cache-and-network"
   });
-
-  // Memoized recipes array
-  const recipes = useMemo(() => {
-    if (!data?.usersBuilds) {
-      return [];
-    }
-    const recipes: Recipe[] = [];
-    data.usersBuilds.forEach(userBuild => {
-      const { recipe } = userBuild;
-      const index = recipes.findIndex(rec => rec.name === recipe.name);
-      if (index === -1) {
-        recipes.push({
-          ...recipe,
-          build: [userBuild]
-        });
-      } else {
-        recipes[index] = {
-          ...recipes[index],
-          build: [...recipes[index].build, { ...userBuild }]
-        };
-      }
-    });
-
-    recipes.sort((a, b) => a.name.localeCompare(b.name));
-    return recipes;
-  }, [data?.usersBuilds]);
+  const recipeList = useReactiveVar(userRecipeList);
 
   useEffect(() => {
-    userRecipeList(recipes);
-  }, [recipes]);
+    if (data) {
+      const recipes = convertRecipes(data);
+      recipes.sort((a, b) => a.name.localeCompare(b.name));
+      userRecipeList(recipes);
+    } else {
+      console.log("no data, refetching");
+      refetch();
+    }
+  }, [data, refetch]);
 
   if (loading) {
     return <div>Loading...</div>;
   }
+
   if (error) {
     return <div>{error.message}</div>;
   }
 
-  return <div>{`${recipes.length} Recipes Loaded`}</div>;
+  return <div>{`${recipeList.length} Recipes Loaded`}</div>;
+}
+
+function convertRecipes(data: { usersBuilds: Build[] }) {
+  const recipes: Recipe[] = [];
+  data.usersBuilds.forEach(userBuild => {
+    const { recipe } = userBuild;
+    const index = recipes.findIndex(rec => rec.name === recipe.name);
+    if (index === -1) {
+      recipes.push({
+        ...recipe,
+        build: [userBuild]
+      });
+    } else {
+      recipes[index] = {
+        ...recipes[index],
+        build: [...recipes[index].build, { ...userBuild }]
+      };
+    }
+  });
+  recipes.sort((a, b) => a.name.localeCompare(b.name));
+  return recipes;
 }
