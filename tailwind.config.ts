64e46120d29a38a4a6e80a9b431b992700dee56a
jacob-af <jfeitler@gmail.com>
2024-05-09 09:29:01 -0400

    image upload started

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
+import { withUt } from "uploadthing/tw";
 
-const config: Config = {
+export default withUt({
+  // Your existing Tailwind config
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +16,21 @@ const config: Config = {
     }
   },
   plugins: []
-};
-export default config;
+});
+
+// const config: Config = {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+//   ],
+//   theme: {
+//     extend: {
+//       fontSize: {
+//         xxs: "10px" // Define smaller font size here
+//       }
+//     }
+//   },
+//   plugins: []
+// };
+// export default config;
