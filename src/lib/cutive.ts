c58d4bbe6d58aa7b4c697829a470fbfd76d5303d
jacob-af <jfeitler@gmail.com>
2024-05-24 15:09:35 -0400

    minor css font changes

--- src/lib/cutive.ts
+++ src/lib/cutive.ts
@@ -1,6 +1,11 @@
-import { Cutive, Permanent_Marker } from "next/font/google";
+import {
+  Cutive,
+  Limelight,
+  Permanent_Marker,
+  Poiret_One
+} from "next/font/google";
 
-export const cutive = Permanent_Marker({
+export const cutive = Poiret_One({
   subsets: ["latin"],
   weight: "400",
   preload: false
