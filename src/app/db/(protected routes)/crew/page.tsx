99fb9c58bb077d9d44a0839a986d480c4f7edce0
jacob-af <jfeitler@gmail.com>
2024-05-24 12:43:08 -0400

    removed unneeded file upload

--- src/app/db/(protected routes)/crew/page.tsx
+++ src/app/db/(protected routes)/crew/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import UploadFile from "@/components/SharedComponents/FileUpload";
 import { UserList } from "@/components/crew/UserList";
 import UserLoader from "@/components/crew/UserLoader";
 
 export default function Recipe() {
   return (
-    <div className="flex flex-col content-center align-middle max-w-md min-w-sm m-20 box-border">
+    <div className="min-w-sm m-20 box-border flex max-w-md flex-col content-center align-middle">
       Here are all of the users registered and your relationship:
       <UserLoader />
       <UserList />
