ab1dc9f6fa1c0d9ecb9f64749035dc58f5fa0356
jacob-af <jfeitler@gmail.com>
2024-06-02 09:45:05 -0400

    remove bad navbar

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -3,9 +3,8 @@ import { PublicNavBar } from "@/components/navigation/PublicNavBar";
 export default function NewLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <main className="flex w-full flex-col items-center justify-center pt-16">
+      <main className="flex w-full flex-col items-center justify-center">
         {children}
-        <PublicNavBar />
       </main>
     </>
   );
