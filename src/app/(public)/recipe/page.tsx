a5dce8e5a723a7013d5e3f1222046dd26039bb03
jacob-af <jfeitler@gmail.com>
2024-06-02 09:44:22 -0400

    removed unneccesary fragments

--- src/app/(public)/recipe/page.tsx
+++ src/app/(public)/recipe/page.tsx
@@ -1,12 +1,11 @@
+import Link from "next/link";
 import { PublicRecipeBox } from "@/components/recipe/display/PublicRecipeBox";
 import RecipeLoader from "@/components/recipe/PublicRecipeLoader";
 
 export default function PublicRecipePage() {
   return (
-    <>
-      <div className="mt-10 box-border grid h-full w-full max-w-7xl grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
-        <PublicRecipeBox />
-      </div>
-    </>
+    <div className="mt-10 box-border grid h-full w-full max-w-7xl grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+      <PublicRecipeBox />
+    </div>
   );
 }
