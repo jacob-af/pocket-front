8b33a615c397e7579a34a474b906794ddd4ca07f
jacob-af <jfeitler@gmail.com>
2024-06-02 09:44:42 -0400

    more aesthetically pleasing weclome

--- src/app/(public)/welcome/page.tsx
+++ src/app/(public)/welcome/page.tsx
@@ -1,17 +1,22 @@
 import AuthButton from "@/components/buttons/SignOutButton";
+import { BreakPoint } from "@/components/images/CardBorder";
 import Link from "next/link";
 export default function Home() {
   return (
-    <main className="flex h-full w-full max-w-xl flex-col items-center text-center">
-      <h1 className="mb-4 text-4xl font-bold">
-        Welcome to the Pocket Bar Book
-      </h1>
-      <p className="mb-8">
+    <main className="flex min-h-screen w-screen flex-col items-center justify-center overflow-hidden text-center">
+      <h1 className="text-4xl font-bold">Welcome to the Pocket Bar Book</h1>
+      <p className="my-4">
         A recipe management app designed for working bartenders. Keep track of
         different builds, version control for recipe development. Share with
         your team. Manage inventory and run cost. Put it all in your Pocket.
       </p>
-      <div className="flex flex-row items-center justify-center">
+      <Link href="/recipe" passHref className="my-8 text-xl">
+        Search Public Recipes
+      </Link>
+      <div>
+        <BreakPoint />
+      </div>
+      <div className="my-8 flex flex-row items-center justify-center">
         <Link
           href="signup"
           className="btn-primary mx-3 inline-block rounded bg-white p-2 text-black hover:bg-gray-300"
