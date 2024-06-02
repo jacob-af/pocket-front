667fba67d228b15427fda2387bb68b65139dba51
jacob-af <jfeitler@gmail.com>
2024-06-02 09:49:15 -0400

    moved to client with lazy loading

--- src/components/recipe/display/PublicRecipeBox.tsx
+++ src/components/recipe/display/PublicRecipeBox.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   ApolloQueryResult,
   useLazyQuery,
@@ -8,51 +10,84 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 
 import PublicCard from "@/components/recipe/display/PublicCard";
 import { Recipe } from "@/__generated__/graphql";
-import { getClient } from "@/lib/client";
+import { useLazyLoad } from "@/hooks/useLazyLoad";
 
-export async function PublicRecipeBox() {
-  const client = await getClient();
-  try {
-    const { data }: ApolloQueryResult<{ publicRecipes: Recipe[] }> =
-      await client.query({
-        query: PUBLIC_RECIPES,
-        context: {}
-      });
-    const columnConfigurations = [[1], [2, 2], [3, 3, 3]];
-    console.log(data, ": loading data");
-    return (
-      <>
-        {columnConfigurations.map((columns, index) =>
-          //{/* Create a div for each column configuration */}
-          columns.map((num, columnIndex) => (
-            <div
-              key={`${index}-${columnIndex}`}
-              className={`col-span-1 justify-items-center w-full ${
-                index === 0 ? "grid md:hidden" : ""
-              } ${index === 1 ? "hidden md:grid xl:hidden" : ""}${
-                index === 2 ? "hidden xl:grid" : ""
-              }`}
-            >
-              {/* Calculate the starting index for this column's builds */}
-              {data.publicRecipes
-                .filter((build, i) => build && i % num === columnIndex)
-                .map(
-                  (recipe, index) =>
-                    recipe && (
-                      <PublicCard
-                        key={recipe.id + index}
-                        recipe={recipe}
-                        index={index}
-                      />
-                    )
-                )}
-            </div>
-          ))
-        )}
-      </>
-    );
-  } catch (err) {
-    console.log(err);
-    return <div>Something has gone Wrong</div>;
-  }
+export function PublicRecipeBox() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const [recipeSet, setRecipeSet] = useState<Set<string>>(new Set());
+  const [recipeList, setList] = useState<Recipe[]>([]);
+  const itemsPerPage = 12;
+
+  const [getData, { loading, error }] = useLazyQuery(PUBLIC_RECIPES, {
+    onCompleted: response => {
+      const newRecipes = response.publicRecipes.filter(
+        recipe => !recipeSet.has(recipe.id)
+      );
+      if (newRecipes.length > 0) {
+        setRecipeSet(
+          prevSet =>
+            new Set([
+              ...Array.from(prevSet),
+              ...newRecipes.map(recipe => recipe.id)
+            ])
+        );
+        setList(value => [...value, ...newRecipes]);
+        setHasMore(newRecipes.length === itemsPerPage);
+      } else {
+        setHasMore(false);
+      }
+    }
+  });
+
+  useEffect(() => {
+    getData({
+      variables: { skip: currentPage * itemsPerPage, take: itemsPerPage }
+    });
+  }, [currentPage]);
+
+  const [observerRef] = useLazyLoad(() => {
+    if (hasMore && !loading) {
+      setCurrentPage(prevPage => prevPage + 1);
+    }
+  });
+
+  const columnConfigurations = [[1], [2, 2], [3, 3, 3]];
+
+  return (
+    <>
+      {columnConfigurations.map((columns, index) =>
+        // Create a div for each column configuration
+        columns.map((num, columnIndex) => (
+          <div
+            key={`${index}-${columnIndex}`}
+            className={`col-span-1 justify-items-center w-full ${
+              index === 0 ? "grid md:hidden" : ""
+            } ${index === 1 ? "hidden md:grid xl:hidden" : ""}${
+              index === 2 ? "hidden xl:grid" : ""
+            }`}
+          >
+            {/* Calculate the starting index for this column's builds */}
+            {recipeList
+              .filter((build, i) => build && i % num === columnIndex)
+              .map(
+                (recipe, index) =>
+                  recipe && (
+                    <PublicCard
+                      key={recipe.id + index}
+                      recipe={recipe}
+                      index={index}
+                    />
+                  )
+              )}
+          </div>
+        ))
+      )}
+      {/* Add a div to observe for lazy loading */}
+      <div
+        ref={observerRef as React.RefObject<HTMLDivElement>}
+        style={{ height: "20px", background: "transparent" }}
+      />
+    </>
+  );
 }
