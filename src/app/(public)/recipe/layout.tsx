90514e619d637240e07cb7dc7130dbae4611487e
jacob-af <jfeitler@gmail.com>
2024-06-02 09:43:58 -0400

    improved navigation

--- src/app/(public)/recipe/layout.tsx
+++ src/app/(public)/recipe/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import RecipeLoader from "@/components/recipe/PublicRecipeLoader";
 
 export default async function Dashboard({
@@ -6,10 +7,22 @@ export default async function Dashboard({
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex min-h-screen w-screen flex-col items-center overflow-hidden pt-12">
+    <main className="flex min-h-screen w-screen flex-col items-center overflow-hidden">
       <RecipeLoader />
-
       {children}
+      <div className="bg-background fixed top-0 z-20 flex w-full justify-between p-2">
+        <Link href="/welcome" className="p-2">
+          Pocket
+        </Link>
+        <div className="flex space-x-4">
+          <Link href="/signup" className="p-2">
+            Sign Up
+          </Link>
+          <Link href="/login" className="p-2">
+            Login
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
