2e716e2d08bb695a9fe15dea030a0e2fbde6bcb3
jacob-af <jfeitler@gmail.com>
2024-06-12 19:16:04 -0400

    moved use effect to sign out

--- src/components/images/ProfilePictureSmall.tsx
+++ src/components/images/ProfilePictureSmall.tsx
@@ -1,26 +1,13 @@
 "use client";
 
-import { getSession, useSession } from "next-auth/react";
-
 import { ProfileImage } from "./ProfileImage";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 export default function AmILoggedIn() {
-  const { data: session, update } = useSession();
-
-  useEffect(() => {
-    async function fetchSession() {
-      const sess = await getSession();
-      if (sess?.user && sess.user.accessTokenExpires < Date.now()) {
-        update({ action: "New Tokens" });
-      }
-    }
-    fetchSession();
-  });
+  const { data: session } = useSession();
 
   return (
-    <div className="fixed left-2 top-2">
+    <div className="fixed left-2 top-2 flex flex-col items-center justify-center">
       {session?.user.name ? session.user.name : "not loaded"}
       <ProfileImage url={session?.user.image || "/portrait-placeholder.png"} />
     </div>
