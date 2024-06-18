00079a29eacd7fad97ddbd2e2657099ab52b9d1e
jacob-af <jfeitler@gmail.com>
2024-06-18 03:39:19 -0400

    merging arrays

--- src/components/recipe/display/PublicCard.tsx
+++ src/components/recipe/display/PublicCard.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import {} from "@/components/images/Images";
-
-import { Build, Recipe, Touch } from "@/__generated__/graphql";
+import { Build, Recipe } from "@/__generated__/graphql";
 import { useEffect, useState } from "react";
 
 import { CardBorder } from "@/components/images/CardBorder";
 import { CocktailPicture } from "@/components/images/CocktailPicture";
-import TempImage from "@/components/images/Images";
 import { selectedRecipe } from "@/graphql/reactiveVar/recipes";
 import { useRouter } from "next/navigation";
 
@@ -19,13 +16,31 @@ export default function PublicCard({
   index: number;
 }) {
   const router = useRouter();
-  const [url, setUrl] = useState("/withcherry100.png");
+  const [builds, setBuilds] = useState<Build[]>([]);
+
+  function mergeArrays<Build>(
+    array1: (Build | null)[] | null,
+    array2: (Build | null)[] | null
+  ): Build[] {
+    // Handle cases where either array is null
+    const a1 = (array1 || []).filter((item): item is Build => item !== null);
+    const a2 = (array2 || []).filter((item): item is Build => item !== null);
+
+    // Create a new set to store unique values
+    const uniqueValues = new Set<Build>([...a1, ...a2]);
+
+    // Convert the set back to an array
+    return Array.from(uniqueValues);
+  }
 
   useEffect(() => {
-    // Update the URL state when the current build index or builds change
-    const build = recipe.publicBuild?.[0];
-    const imageUrl = build?.image ?? "/withcherry100.png";
-    setUrl(imageUrl);
+    if (recipe) {
+      const mergedBuilds = mergeArrays(
+        recipe.publicBuild || [],
+        recipe.userBuild || []
+      );
+      setBuilds(mergedBuilds);
+    }
   }, [recipe]);
 
   const handleView = () => {
@@ -33,9 +48,11 @@ export default function PublicCard({
     router.push(`/recipe/${recipe.name}`);
   };
 
+  console.log(builds);
+
   return (
-    <div className="bg-contrast h-public-card-lg relative my-2 w-full content-center rounded-lg text-center">
-      {/* THis goes on bottom */}
+    <div className="bg-contrast h-public-card-lg relative my-2 box-border w-full max-w-lg content-center rounded-lg text-center">
+      {/* This goes on bottom */}
       <div className="absolute inset-0 z-0 h-full w-full">
         <CardBorder />
       </div>
@@ -43,8 +60,10 @@ export default function PublicCard({
       <div className="relative z-10 h-full p-8">
         <div className="p-2 text-center text-xl">{recipe.name}</div>
         <div className="block">
-          <div className="float-left mr-2 w-32 lg:w-auto">
-            <CocktailPicture url={url} />{" "}
+          <div className="float-left mr-2 w-32">
+            <CocktailPicture
+              url={builds[0]?.image ? builds[0].image : "/withcherry100.png"}
+            />{" "}
           </div>
           <div className="mt-4">{recipe.about}</div>
         </div>
@@ -55,10 +74,8 @@ export default function PublicCard({
         >
           <div>Explore Recipe</div>
           <div>
-            {recipe.publicBuild?.length} Build
-            {!!recipe.publicBuild && recipe.publicBuild?.length > 1
-              ? "s "
-              : " "}
+            {builds.length} Build
+            {!!builds && builds.length > 1 ? "s " : " "}
             Available
           </div>
         </button>
