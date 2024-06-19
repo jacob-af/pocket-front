46e90d38915b1aea1c9d9eddb48c0dc3815bf99f
jacob-af <jfeitler@gmail.com>
2024-06-19 05:36:02 -0400

    logging sessions status in profile load

--- src/components/images/ProfilePictureSmall.tsx
+++ src/components/images/ProfilePictureSmall.tsx
@@ -6,23 +6,24 @@ import { ProfileImage } from "./ProfileImage";
 import { useEffect } from "react";
 
 export default function AmILoggedIn() {
-  const { data: session, update } = useSession();
+  const { data: session, update, status } = useSession();
 
   useEffect(() => {
     async function fetchSession() {
       const sess = await getSession();
-      console.log(sess, "profile pic use effect");
+      console.log(sess, "use effect");
       if (sess?.user && sess.user.accessTokenExpires < Date.now()) {
+        console.log("new tokens");
         update({ action: "New Tokens" });
       }
     }
     fetchSession();
   });
 
-  console.log(session, ": profile picture");
+  console.log(session, status, ": profile picture");
   return (
     <div className="fixed left-2 top-2 flex flex-col items-center justify-center">
-      {session?.user.name ? session.user.name : "not loaded"}
+      {status === "authenticated" ? session.user.name : "not loaded"}
       <ProfileImage url={session?.user.image || "/portrait-placeholder.png"} />
     </div>
   );
