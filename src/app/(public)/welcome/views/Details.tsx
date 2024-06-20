8388afc49346ccd2fb5ccc2ed6fdae4e49dfa11e
jacob-af <jfeitler@gmail.com>
2024-06-20 05:14:59 -0400

    welcome page enhancement

--- src/app/(public)/welcome/views/Details.tsx
+++ src/app/(public)/welcome/views/Details.tsx
@@ -1,7 +1,13 @@
 export default function Details() {
   return (
-    <div className="flex min-h-screen w-screen snap-center items-center justify-center">
-      Page 3
+    <div className="flex min-h-screen w-screen snap-center items-center justify-center text-2xl">
+      <ul>
+        <li>Upload Photos of your Cocktails</li>
+        <li>Import and Export Recipe Books as CSV</li>
+        <li>Download App to Home Screen</li>
+        <li>Contact Admin to Sign in with Google</li>
+        <li>More Features Coming Soon</li>
+      </ul>
     </div>
   );
 }
--- src/app/(public)/welcome/views/Welcome.tsx
+++ src/app/(public)/welcome/views/Welcome.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 export default function Welcome() {
   return (
-    <div className="box-border flex min-h-screen w-screen max-w-2xl snap-center flex-col items-center justify-center place-self-center">
-      <h1 className="text-center text-6xl font-bold">Welcome</h1>
-      <h2 className="text-center text-2xl font-bold"> to the </h2>
-      <h1 className="text-center text-8xl font-bold">Pocket Bar Book</h1>
+    <div className="box-border flex min-h-screen w-screen max-w-2xl snap-center flex-col items-center justify-center p-4 text-center">
+      <h1 className="text-6xl font-bold">Welcome</h1>
+      <h2 className="text-2xl font-bold"> to the </h2>
+      <h1 className="text-8xl font-bold">Pocket Bar Book</h1>
       <p className="my-4">
         A recipe management app designed for working bartenders. Keep track of
         different builds, version control for recipe development. Share with
