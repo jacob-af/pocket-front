d7f1dad6193b6e6e76c82d32301d8299f3711a4c
jacob-af <jfeitler@gmail.com>
2024-06-28 14:50:20 -0400

    privacy policy page

---
+++ src/app/(public)/privacy/page.tsx
@@ -0,0 +1,61 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="flex w-full max-w-2xl pt-20 text-center">
+      Privacy Policy Last Updated: 6/28/2024 <br />
+      1. Introduction Welcome to Pocket Bar Book (the "App"). We are committed
+      to protecting your privacy and ensuring the security of your personal
+      information. This Privacy Policy explains how we collect, use, disclose,
+      and safeguard your information when you use our App and services. <br />
+      2. Information We Collect We may collect personal information from you in
+      a variety of ways, including, but not limited to, when you use our App,
+      register on the App, log in, and in connection with other activities,
+      services, features, or resources we make available on our App. The
+      personal information we may collect includes: Name Email address Login
+      credentials (username and password) IP address Browser type and version
+      Time zone settings Operating system and platform Recipe submissions and
+      preferences <br />
+      3. How We Use Your Information We may use the information we collect from
+      you in the following ways: To personalize your experience on our App To
+      improve our App and services To process your transactions and manage your
+      account To send periodic emails regarding your account or other products
+      and services To respond to your inquiries, requests, or questions To share
+      and display your submitted recipes with other users (with your consent){" "}
+      <br />
+      4. How We Protect Your Information We implement a variety of security
+      measures to maintain the safety of your personal information. Your
+      personal information is contained behind secure networks and is only
+      accessible by a limited number of persons who have special access rights
+      to such systems and are required to keep the information confidential. All
+      sensitive information you supply is encrypted via Secure Socket Layer
+      (SSL) technology. <br />
+      5. Sharing Your Information We do not sell, trade, or otherwise transfer
+      to outside parties your personally identifiable information unless we
+      provide users with advance notice. This does not include App hosting
+      partners and other parties who assist us in operating our App, conducting
+      our business, or serving our users, so long as those parties agree to keep
+      this information confidential. We may also release information when it's
+      release is appropriate to comply with the law, enforce our App policies,
+      or protect ours or others' rights, property, or safety. <br />
+      6. Third-Party Links Occasionally, at our discretion, we may include or
+      offer third-party products or services on our App. These third-party sites
+      have separate and independent privacy policies. We, therefore, have no
+      responsibility or liability for the content and activities of these linked
+      sites. Nonetheless, we seek to protect the integrity of our App and
+      welcome any feedback about these sites. <br />
+      7. Cookies Our App may use "cookies" to enhance the user experience. Your
+      web browser places cookies on your hard drive for record-keeping purposes
+      and sometimes to track information about you. You may choose to set your
+      web browser to refuse cookies or to alert you when cookies are being sent.
+      If you do so, note that some parts of the App may not function properly.
+      <br />
+      8. Your Consent By using our App, you consent to our App's Privacy Policy.
+      <br />
+      9. Changes to Our Privacy Policy If we decide to change our privacy
+      policy, we will post those changes on this page, and/or update the Privacy
+      Policy modification date above. Policy changes will apply only to
+      information collected after the date of the change. 10. Contacting Us If
+      there are any questions regarding this privacy policy, you may contact us
+      using the information below: Pocket Bar Book
+    </div>
+  );
+}
