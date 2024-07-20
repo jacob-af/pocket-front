0007959bbf5a797089e256fb02984ec257c85b19
jacob-af <jfeitler@gmail.com>
2024-07-20 19:04:46 -0400

    removed add recipe button

--- src/app/db/(protected routes)/recipe/page.tsx
+++ src/app/db/(protected routes)/recipe/page.tsx
@@ -1,14 +1,8 @@
-import AddRecipe from "@/components/buttons/AddRecipeButton";
 import { RecipeBox } from "@/components/recipe/display/UserRecipeBox";
-import RecipeLoader from "@/components/recipe/UserRecipeLoader";
 
 export default async function RecipePage() {
   return (
     <>
-      <div className="fixed right-2 top-12 z-40">
-        <AddRecipe />
-      </div>
-
       <RecipeBox />
     </>
   );
